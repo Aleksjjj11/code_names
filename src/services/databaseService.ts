4fed39893842fb8727e6d18c4677cb5d3a93d846
anrys <anrysea@gmail.com>
2023-07-29 14:43:47 +0900

    renderLc and autoComplete refactor

--- src/services/databaseService.ts
+++ src/services/databaseService.ts
@@ -2,6 +2,7 @@ import sqlite3 from "sqlite3";
 import {open} from "sqlite"
 import User from "database_models/User";
 import Dictionary from "database_models/Dictionary";
+import AutoCompleteData from "database_models/AutoCompleteData";
 
 export default class DatabaseService {
     private readonly databasePath: string;
@@ -34,20 +35,45 @@ export default class DatabaseService {
         const query = "INSERT INTO dicts VALUES(null, ?, ?, ?, 0)";
         const db = await this.openDb();
         await db.run(query, request.session.uid, request.body.name, JSON.stringify(words));
-        response.send({type: 'redirect', url: '/lc/1'});
     }
 
     public async refreshPacInDb(request: any, words: string[], response: any, id: number) {
         const query = "UPDATE dicts SET name = ?, words = ? where id = ?";
         const db = await this.openDb();
         await db.run(query, request.body.name, JSON.stringify(words), id);
-        response.send({text: "Ваш пак обновлён!"});
     }
 
     public async getPacById(id: string): Promise<Dictionary | undefined> {
         const query = "SELECT * FROM dicts where id = ?";
         const db = await this.openDb();
         return await db.get<Dictionary>(query, id);
+    }
+
+
+    public async autoComplete(value: string): Promise<AutoCompleteData[] | string> {
+        const partAuto = "%" + value + "%";
+        const query = "SELECT id , name FROM dicts WHERE name LIKE ? ORDER BY name LIMIT 10";
+        const db = await this.openDb();
+        const results = await db.all<AutoCompleteData[]>(query, partAuto);
+
+        if (results.length > 0) {
+            return results;
+        } else {
+            return "/0";
+        }
+    }
+
+    public async getCountByUid(uid: number): Promise<number> {
+        const db = await this.openDb();
+        const query = "SELECT count (*) as count FROM dicts where uid = ? ";
+        const result = await db.get(query, uid);
+        return result.count;
+    }
+
+    public async getDictsByUidWithPagination(uid: number, offset: number, limit: number): Promise<Dictionary[]> {
+        const db = await this.openDb();
+        const query = "SELECT * FROM dicts WHERE uid = ? ORDER BY id DESC LIMIT ?, ?";
+        return await db.all<Dictionary[]>(query, uid, offset, limit);
     }
 
     private async openDb () {
