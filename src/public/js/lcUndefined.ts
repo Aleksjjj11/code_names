ea444e4ec4e621487025006b61a89779ba88ea7e
alekseyalekseev <aleks2000_25@mail.ru>
2023-08-13 17:24:11 +0900

    [+] Hot recompile the code (not working for new created CSS styles) [=] Prevented the sending a request to backend if the login properties are not filled

--- src/public/js/lcUndefined.ts
+++ src/public/js/lcUndefined.ts
@@ -1,9 +1,43 @@
-let butt = document.getElementById("send") as HTMLButtonElement;
+const loginButton = document.getElementById("send") as HTMLButtonElement;
+const loginForm = document.getElementById("loginForm") as HTMLFormElement;
+const loginInput = document.getElementById("log") as HTMLInputElement;
+const passwordInput = document.getElementById("pas") as HTMLInputElement;
 
-butt.onclick = () => {
-    let login = (document.querySelector("#log") as HTMLInputElement).value;
-    let pass = (document.querySelector("#pas") as HTMLInputElement).value;
-    post("/lcLogin", {login: login, password: pass}, (resp) => {
+loginForm.onsubmit = (ev: Event) => {
+    ev.preventDefault();
+}
+
+loginInput.oninput = (ev: Event) => {
+    ev.preventDefault();
+    if (loginInput.classList.contains("is-invalid")) {
+        loginInput.classList.remove("is-invalid");
+    }
+}
+
+passwordInput.oninput = (ev: Event) => {
+    ev.preventDefault();
+    if (passwordInput.classList.contains("is-invalid")) {
+        passwordInput.classList.remove("is-invalid");
+    }
+}
+
+loginButton.onclick = () => {
+    const login = loginInput.value;
+    const password = passwordInput.value;
+
+    if (!login) {
+        loginInput.classList.add("is-invalid");
+    }
+
+    if (!password) {
+        passwordInput.classList.add("is-invalid");
+    }
+
+    if (!login || !password) {
+        return;
+    }
+
+    post("/lcLogin", {login: login, password: password}, (resp) => {
         let data = JSON.parse(resp.responseText);
         if (data.type === "redirect") {
             document.location = data.url;
